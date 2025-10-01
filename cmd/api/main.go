09b1db615dfb8356cf742374b2cfdfc179a54212
Errol Vas <76538965+errol-vas@users.noreply.github.com>
2025-10-01 23:54:35 +0100

    feat: added config package to load environment variables (#3) (#4)
    Signed-off-by: errol-vas <errolvas@live.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,10 +6,15 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/errol-vas/shiftplanner/internal/config"
 	"github.com/errol-vas/shiftplanner/internal/handlers"
 )
 
 func main() {
+
+	// Load Config
+	cfg := config.Load()
+	log.Printf("Starting application in %s mode on port %s", cfg.Env, cfg.Port)
 
 	// Set server to healthy
 	atomic.StoreInt32(&handlers.Health, 1)
@@ -18,7 +23,7 @@ func main() {
 	http.HandleFunc("/api/health", handlers.HealthCheck)
 
 	server := &http.Server{
-		Addr:         ":5000",
+		Addr:         ":" + cfg.Port,
 		Handler:      nil,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
@@ -26,6 +31,6 @@ func main() {
 	}
 
 	// Start the Server
-	log.Println("Starting server on : 5000")
+	log.Printf("Starting server on port %s", cfg.Port)
 	log.Fatal(server.ListenAndServe())
 }
