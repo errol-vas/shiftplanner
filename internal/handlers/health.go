42d8ec22e0f2ef07c0db5b9ffa4586073a4a5894
Errol Vas <76538965+errol-vas@users.noreply.github.com>
2025-10-01 23:19:09 +0100

    refactor: updated healthcheck and folder structure (#2)
    Signed-off-by: errol-vas <errolvas@live.com>

--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -1,8 +1,19 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"sync/atomic"
+)
+
+var Health int32
 
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w.Header().Set("Content-Type", "application/json")
+	if atomic.LoadInt32(&Health) == 1 {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"available"}`))
+		return
+	}
+	w.WriteHeader(http.StatusServiceUnavailable)
+	w.Write([]byte(`{"status":"unavailable"}`))
 }
