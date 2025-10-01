bb48453b99223f39d11dc23efb5e04621fcc269b
errol-vas <errolvas@live.com>
2025-10-01 23:14:34 +0100

    refactor: updated healthcheck and folder structure
    Signed-off-by: errol-vas <errolvas@live.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,19 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
+	"time"
 
 	"github.com/errol-vas/shiftplanner/internal/handlers"
 )
 
-
-
-
 func main() {
+
+	// Set server to healthy
+	atomic.StoreInt32(&handlers.Health, 1)
 
 	// API Routes
 	http.HandleFunc("/api/health", handlers.HealthCheck)
 
+	server := &http.Server{
+		Addr:         ":5000",
+		Handler:      nil,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
 	// Start the Server
 	log.Println("Starting server on : 5000")
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	log.Fatal(server.ListenAndServe())
 }
