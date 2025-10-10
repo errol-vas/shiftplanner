8a2415f771580841286980d986e5cbdd87d488e3
errol-vas <errolvas@live.com>
2025-10-10 14:29:45 +0100

    feat: log ip addr, user-agent and referer
    Signed-off-by: errol-vas <errolvas@live.com>

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"net"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/errol-vas/shiftplanner/internal/logger"
@@ -19,12 +21,21 @@ func Logging(next http.Handler) http.Handler {
 
 		reqID := GetRequestID(r.Context())
 
-		logger.Info(
-			"[RequestID: " + reqID + "] " +
-				r.Method + " " + r.URL.Path +
-				" - Status: " + http.StatusText(rw.statusCode) +
-				" (" + duration.String() + ")",
-		)
+		// Client Metadata
+		ip := extractIP(r)
+		userAgent := r.Header.Get("User-Agent")
+		referer := r.Header.Get("Referer")
+
+		logLine := "[RequestID: " + reqID + "] " +
+			r.Method + " " + r.URL.Path +
+			" - Status: " + http.StatusText(rw.statusCode) +
+			" (" + duration.String() + ")" +
+			" - IP: " + ip +
+			" - User-Agent: " + userAgent
+		if referer != "" {
+			logLine += " - Referer: " + referer
+		}
+		logger.Info(logLine)
 	})
 }
 
@@ -36,4 +47,20 @@ type responseWriter struct {
 func (rw *responseWriter) WriteHeader(code int) {
 	rw.statusCode = code
 	rw.ResponseWriter.WriteHeader(code)
+}
+
+func extractIP(r *http.Request) string {
+
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	if xForwardedFor != "" {
+		parts := strings.Split(xForwardedFor, ",")
+		return strings.TrimSpace(parts[0])
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr
+	}
+
+	return ip
 }
